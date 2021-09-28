4113d14420a99b28f52a71f6af88cb02184a26a7
AnastasiaMorozovaBPI205 <avmorozova_6@edu.hse.ru>
2021-09-28 23:05:51 +0300

    sample commit

--- Presentation_CoreData/ViewController.swift
+++ Presentation_CoreData/ViewController.swift
@@ -7,13 +7,60 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+           let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")!
+        
+           cell.textLabel!.text = names[indexPath.row]
+        
+         return cell
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return names.count
+    }
+    
+    
 
+    @IBAction func addName(_ sender: Any) {
+        let alert = UIAlertController(
+            title: "New name",
+            message: "Add a new name",
+            preferredStyle: .alert
+        )
+         
+        let saveAction = UIAlertAction(title: "Save", style: .default) {
+            (action: UIAlertAction!) -> Void in
+         
+            let textField = alert.textFields![0]
+            self.names.append(textField.text!)
+            self.tableView.reloadData()
+        }
+         
+        let cancelAction = UIAlertAction(title: "Cancel", style: .default) {
+            (action: UIAlertAction!) -> Void in
+        }
+         
+        alert.addTextField {(textField: UITextField!) -> Void in }
+         
+        alert.addAction(saveAction)
+        alert.addAction(cancelAction)
+         
+        present(alert, animated: true, completion: nil)
+    }
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var names = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
+        title = "Names List"
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+      }
 
 }
 
