523443ffae6d8c13fb39cf13a0f917b6501bb0fa
AnastasiaMorozovaBPI205 <avmorozova_6@edu.hse.ru>
2021-09-28 23:45:15 +0300

    saving fetching commit

--- Presentation_CoreData/ViewController.swift
+++ Presentation_CoreData/ViewController.swift
@@ -7,23 +7,60 @@
 
 import UIKit
 
+import CoreData
+
 class ViewController: UIViewController, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-           let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")!
-        
-           cell.textLabel!.text = names[indexPath.row]
-        
-         return cell
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")!
+         
+        let person = people[indexPath.row]
+        cell.textLabel!.text = person.value(forKey: "name") as? String
+         
+        return cell
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+      super.viewWillAppear(animated)
+     
+        do {
+            let managedContext = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+            
+            let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName:"Person")
+            
+            if let results = try managedContext.fetch(fetchRequest) as? [NSManagedObject] {
+              people = results
+            }
+            
+        } catch {
+            print(error)
+        }
+        
+    }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return names.count
+        return people.count
     }
     
+    func saveName(name: String) {
+     
+      let managedContext = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+      
+        let entity =  NSEntityDescription.entity(
+            forEntityName: "Person",
+            in: managedContext
+      )
+     
+      let person = NSManagedObject(
+        entity: entity!,
+        insertInto:managedContext
+      )
+     
+      person.setValue(name, forKey: "name")
     
+      people.append(person)
+    }
 
     @IBAction func addName(_ sender: Any) {
         let alert = UIAlertController(
@@ -36,7 +73,7 @@ class ViewController: UIViewController, UITableViewDataSource {
             (action: UIAlertAction!) -> Void in
          
             let textField = alert.textFields![0]
-            self.names.append(textField.text!)
+            self.saveName(name: textField.text!)
             self.tableView.reloadData()
         }
          
@@ -54,7 +91,8 @@ class ViewController: UIViewController, UITableViewDataSource {
     
     @IBOutlet weak var tableView: UITableView!
     
-    var names = [String]()
+    var people = [NSManagedObject]()
+    var name = [NSManagedObject]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
